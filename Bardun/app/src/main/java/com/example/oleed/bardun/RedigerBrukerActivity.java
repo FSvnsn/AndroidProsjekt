81e1dac7a7661a151b8a1248212218480a62cb74
Ole-Edvard Ørebæk <ole.e.orebak@hiof.no>
2018-10-03 14:55:07 +0200

    Added primary activities

---
+++ Bardun/app/src/main/java/com/example/oleed/bardun/RedigerBrukerActivity.java
@@ -0,0 +1,13 @@
+package com.example.oleed.bardun;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RedigerBrukerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rediger_bruker);
+    }
+}
