54ee11ae54f13795b23d90fbad494a9dd3a89ff3
Ole-Edvard Ørebæk <ole.e.orebak@hiof.no>
2018-10-03 21:59:48 +0200

    Tried to create navigation drawer, but get runtime error

--- Bardun/app/src/main/java/no/hiof/oleedvao/bardun/BrukerActivity.java
+++ Bardun/app/src/main/java/no/hiof/oleedvao/bardun/BrukerActivity.java
@@ -1,13 +1,29 @@
 package no.hiof.oleedvao.bardun;
 
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toolbar;
+
+import no.hiof.oleedvao.bardun.fragment.NavigationDrawerFragment;
 
 public class BrukerActivity extends AppCompatActivity {
 
+    private android.support.v7.widget.Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bruker);
+
+        toolbar = findViewById(R.id.toolbarBruker);
+
+        setUpNavigationDrawer();
+    }
+
+    private void setUpNavigationDrawer(){
+        NavigationDrawerFragment navigationDrawerFragment = (NavigationDrawerFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentNavDrawerBruker);
+        DrawerLayout drawerLayout = findViewById(R.id.drawerLayoutBruker);
+
+        navigationDrawerFragment.setUpDrawer(drawerLayout, toolbar);
     }
 }
