fd9fb503f85edd716e8e4bc814566ded095d03c6
Ole-Edvard Ørebæk <ole.e.orebak@hiof.no>
2018-10-11 20:20:58 +0200

    Can upload image from gallary, problem with saving image from camera.

--- Bardun/app/src/main/java/no/hiof/oleedvao/bardun/OpprettTeltplassActivity.java
+++ Bardun/app/src/main/java/no/hiof/oleedvao/bardun/OpprettTeltplassActivity.java
@@ -1,13 +1,112 @@
 package no.hiof.oleedvao.bardun;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class OpprettTeltplassActivity extends AppCompatActivity {
+    private static final int REQUEST_IMAGE_GET = 1000;
+    private static final int REQUEST_TAKE_PHOTO = 2000;
+
+    private ImageView imageView;
+    private String CurrentPhotoPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_opprett_teltplass);
+
+        imageView = findViewById(R.id.imageViewOpprettTeltplass);
+    }
+
+    public void getPicture(View view){
+        //creates implicit intent to get image
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("image/*");
+        startActivityForResult(intent, REQUEST_IMAGE_GET);
+    }
+
+    public void takePicture(View view) {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+                Log.d("PicError","Could not createImageFile");
+
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "no.hiof.oleedvao.bardun.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(requestCode == REQUEST_IMAGE_GET && resultCode == RESULT_OK){
+            try{
+                //get the image uri
+                Uri photoUri = data.getData();
+                //get the image bitmap from the uri
+                Bitmap picture = MediaStore.Images.Media.getBitmap(getContentResolver(), photoUri);
+                //set the image view bitmap to the retrieved image bitmap
+                imageView.setImageBitmap(picture);
+            } catch (IOException e){
+                Toast.makeText(this, "Couldn't get picture", Toast.LENGTH_SHORT).show();
+            }
+        }
+        else if(requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK){
+            try{
+                //get the image uri
+                Uri photoUri = data.getData();
+                //get the image bitmap from the uri
+                Bitmap picture = MediaStore.Images.Media.getBitmap(getContentResolver(), photoUri);
+                //set the image view bitmap to the retrieved image bitmap
+                imageView.setImageBitmap(picture);
+            }catch(IOException e){
+                Toast.makeText(this, "Something went wrong", Toast.LENGTH_SHORT).show();
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        CurrentPhotoPath = image.getAbsolutePath();
+        return image;
     }
 }
