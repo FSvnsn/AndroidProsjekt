4f341701842b0a18e118583c38bb21cea4f6aa17
Fredrik Svendsen <fredrik.n.svendsen@gmail.com>
2018-10-17 19:32:07 +0200

    Retrieval functional

--- Bardun/app/src/main/java/no/hiof/oleedvao/bardun/BrukerActivity.java
+++ Bardun/app/src/main/java/no/hiof/oleedvao/bardun/BrukerActivity.java
@@ -4,6 +4,7 @@ import android.support.annotation.NonNull;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.Toolbar;
@@ -21,7 +22,8 @@ import no.hiof.oleedvao.bardun.fragment.NavigationDrawerFragment;
 public class BrukerActivity extends AppCompatActivity {
 
     private android.support.v7.widget.Toolbar toolbar;
-    private DatabaseReference mDatabase;
+    private FirebaseDatabase mDatabase;
+    private DatabaseReference mDatabaseRef;
     private FirebaseAuth mAuth;
     private FirebaseUser CUser;
     private String UID;
@@ -38,18 +40,20 @@ public class BrukerActivity extends AppCompatActivity {
         txtName = findViewById(R.id.txtName);
         txtEmail = findViewById(R.id.txtEmail);
 
-        mDatabase = FirebaseDatabase.getInstance().getReference();
+        mDatabase = FirebaseDatabase.getInstance();
+        mDatabaseRef = mDatabase.getReference();
         mAuth = FirebaseAuth.getInstance();
         CUser = mAuth.getCurrentUser();
         UID = CUser.getUid();
 
-        ValueEventListener UserListener = new ValueEventListener(){
+        mDatabaseRef.addValueEventListener(new ValueEventListener(){
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                String name = dataSnapshot.child("Name").getValue(String.class);
-                User u1 = dataSnapshot.getValue(User.class);
+                showData(dataSnapshot);
+                //String name = dataSnapshot.child("Users").child("Name").getValue().toString();
+                //User u1 = dataSnapshot.getValue(User.class);
                 //Toast.makeText(getApplicationContext(), u1.Name, Toast.LENGTH_LONG).show();
-                txtName.setText(name);
+                //txtName.setText(name);
                 //txtEmail.setText(u1.Email);
             }
 
@@ -58,16 +62,28 @@ public class BrukerActivity extends AppCompatActivity {
                 System.out.println("The read failed: " + databaseError.getCode());
             }
 
-        };
-
-        FirebaseDatabase.getInstance().getReference(UID).addListenerForSingleValueEvent(UserListener);
+        });
 
 
 
         //Test Data Hardcode
-        //User test1 = new User("Fredrik Svendsen", "a@b.no");
-        //mDatabase.child("users").child(UID).setValue(test1);
+        //User test1 = new User("Fredrik N Svendsen", "b@c.no");
+        //mDatabaseRef.child("users").child(UID).setValue(test1);
         //Toast.makeText(this, "IM HERE", Toast.LENGTH_LONG).show();
+    }
+
+    private void showData(DataSnapshot dataSnapshot) {
+        for(DataSnapshot ds : dataSnapshot.getChildren()){
+            User test1 = new User();
+            test1.setName(ds.child(UID).getValue(User.class).getName());
+            test1.setEmail(ds.child(UID).getValue(User.class).getEmail());
+
+            Log.d("TAG","showData: name: " + test1.getName());
+            Log.d("TAG","showData: email: " + test1.getEmail());
+
+            txtName.setText(test1.getName());
+            txtEmail.setText(test1.getEmail());
+        }
     }
 
     private void setUpNavigationDrawer(){
--- Bardun/app/src/main/java/no/hiof/oleedvao/bardun/User.java
+++ Bardun/app/src/main/java/no/hiof/oleedvao/bardun/User.java
@@ -1,14 +1,30 @@
 package no.hiof.oleedvao.bardun;
 
 public class User {
-    public String Name;
-    public String Email;
+    private String name;
+    private String email;
 
     public User(){
     }
 
-    public User(String Name, String Email){
-        this.Name = Name;
-        this.Email = Email;
+    public User(String name, String email){
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
