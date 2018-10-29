5339e2670bde6c1397863a9182fde7dd9b6fd2ae
Ole-Edvard Ørebæk <ole.e.orebak@hiof.no>
2018-10-29 20:58:53 +0100

    Retrieving image from firebase now functional

--- Bardun/app/src/main/java/no/hiof/oleedvao/bardun/Teltplass.java
+++ Bardun/app/src/main/java/no/hiof/oleedvao/bardun/Teltplass.java
@@ -16,7 +16,7 @@ public class Teltplass {
 
     }
 
-    public Teltplass(String latLng, String navn, String beskrivelse, int underlag, int utsikt, int avstand, Boolean skog, Boolean fjell, Boolean fiske){
+    public Teltplass(String latLng, String navn, String beskrivelse, int underlag, int utsikt, int avstand, Boolean skog, Boolean fjell, Boolean fiske, String imageId){
         this.latLng = latLng;
         this.navn = navn;
         this.beskrivelse = beskrivelse;
--- Bardun/app/src/main/java/no/hiof/oleedvao/bardun/TeltplassActivity.java
+++ Bardun/app/src/main/java/no/hiof/oleedvao/bardun/TeltplassActivity.java
@@ -1,5 +1,9 @@
 package no.hiof.oleedvao.bardun;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
@@ -10,7 +14,11 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -18,6 +26,8 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import no.hiof.oleedvao.bardun.fragment.NavigationDrawerFragment;
 
@@ -26,6 +36,8 @@ public class TeltplassActivity extends AppCompatActivity {
     private android.support.v7.widget.Toolbar toolbar;
     private FirebaseDatabase mDatabase;
     private DatabaseReference mDatabaseRef;
+    private FirebaseStorage mStorage;
+    private StorageReference mStorageReference;
     private FirebaseAuth mAuth;
     private FirebaseUser CUser;
     private String UID;
@@ -43,6 +55,8 @@ public class TeltplassActivity extends AppCompatActivity {
     private Switch switchTeltplassSkog;
     private Switch switchTeltplassFjell;
     private Switch switchTeltplassFiske;
+
+    final long ONE_MEGABYTE = 1024 * 1024;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,8 +82,11 @@ public class TeltplassActivity extends AppCompatActivity {
 
         mDatabase = FirebaseDatabase.getInstance();
         mDatabaseRef = mDatabase.getReference();
+        mStorage = FirebaseStorage.getInstance();
+        mStorageReference = mStorage.getReference();
         mAuth = FirebaseAuth.getInstance();
         CUser = mAuth.getCurrentUser();
+
         UID = CUser.getUid();
         teltplassId = "30p000k30p000";
 
@@ -88,7 +105,6 @@ public class TeltplassActivity extends AppCompatActivity {
     }
 
     private void showData(DataSnapshot dataSnapshot) {
-        Teltplass teltplass = new Teltplass();
         try{
             textViewTeltplassNavn.setText(dataSnapshot.child("teltplasser").child(teltplassId).getValue(Teltplass.class).getNavn());
             textViewTeltplassUnderlag.setText(dataSnapshot.child("teltplasser").child(teltplassId).getValue(Teltplass.class).getUnderlag().toString() + "/10");
@@ -97,9 +113,28 @@ public class TeltplassActivity extends AppCompatActivity {
             switchTeltplassSkog.setChecked(dataSnapshot.child("teltplasser").child(teltplassId).getValue(Teltplass.class).getSkog());
             switchTeltplassFjell.setChecked(dataSnapshot.child("teltplasser").child(teltplassId).getValue(Teltplass.class).getFjell());
             switchTeltplassFiske.setChecked(dataSnapshot.child("teltplasser").child(teltplassId).getValue(Teltplass.class).getFiske());
+
+            String imageId = dataSnapshot.child("teltplasser").child(teltplassId).getValue(Teltplass.class).getImageId();
+            StorageReference imageRef = mStorageReference.child("images/" + imageId);
+
+            imageRef.getBytes(ONE_MEGABYTE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
+                @Override
+                public void onSuccess(byte[] bytes) {
+                    Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+                    imageViewTeltplass.setImageBitmap(bitmap);
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    
+                }
+            });
+
+
+
         }
         catch(NullPointerException e){
-
+            Toast.makeText(this, "Something wrong", Toast.LENGTH_LONG).show();
         }
 
 
